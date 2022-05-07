d4e90341e7bb7abaab507703caa06bfb4e55be2c
Cameron Aziz <cameronaziz@me.com>
2022-05-06 23:01:46 -0700

    scripts

--- app/root.tsx
+++ app/root.tsx
@@ -64,6 +64,12 @@ export default function App() {
         <Scripts />
         <LiveReload />
       </body>
+      <script src="assets/js/jquery-3.2.1.min.js"></script>
+      <script src="assets/js/popper.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <script src="assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
+      <script src="assets/plugins/slider/js/owl.carousel.min.js"></script>
+      <script src="assets/js/script.js"></script>
     </html>
   );
 }
